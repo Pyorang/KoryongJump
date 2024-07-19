70e1932fead7accd0307c1f0e5f545ea86a00e1f
상빈 <67950060+17642@users.noreply.github.com>
2024-07-19 20:58:32 +0900

    상점UI구성

---
+++ Assets/Script/ShopUIScript.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class ShopUIScript : MonoBehaviour
+{
+    [SerializeField]
+    public Button GoToMainButton;
+    [SerializeField]
+    public Button NextCharacterSelectButton;
+    [SerializeField]
+    public Button PreviousCharacterSelectButton;
+
+    [SerializeField]
+    public Image SelectedCharacterSprite;
+    [SerializeField]
+    public Image LockPlayerImage;
+
+    [SerializeField]
+    public TMPro.TextMeshProUGUI PerkDescreption;
+    [SerializeField]
+    public TMPro.TextMeshProUGUI CharacterName;
+
+    [SerializeField]
+    public Image SelectedPerkA;
+    [SerializeField]
+    public Image SelectedPerkB;
+
+    [SerializeField]
+    public Button BuyButton;
+
+    public object PlayerData;//CurrentSelectedCharacterIndex, CharacterUnlockedList, PerkUnlockedList, CurrentCoin를 가져온다
+
+
+    public Image[] CharacterImages;
+    public Image[] PerkImages;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
