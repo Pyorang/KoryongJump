2a0eac51f241b63b563211ee0a2380d43a763e8f
상빈 <67950060+17642@users.noreply.github.com>
2024-08-10 16:45:37 +0900

    설정 창 구현

--- Assets/Script/SettingScreenScript.cs
+++ Assets/Script/SettingScreenScript.cs
@@ -1,18 +1,104 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Unity.VisualScripting;
 
 public class SettingScreenScript : MonoBehaviour
 {
+
+    [SerializeField]
+    public int[] QualityOffset = { 1, 3, 5 };
+
+    [SerializeField]
+    public Slider VolumeSlider;
+    [SerializeField]
+    public Slider QualitySlider;
+    [SerializeField]
+    public Toggle MuteToggle;
+    [SerializeField]
+    public Toggle VibrateToggle;
+    [SerializeField]
+    public GameObject BG;
+
+    public int QualityValue;
+    public float VolumeValue;
+
+    public bool MuteToggleEnabled;
+    public bool VibrateToggleEnabled;
+
     // Start is called before the first frame update
-    void Start()
+    public void OnEnable()
     {
-        
+        GetData();
+        UpdateUI();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void GetData()
     {
-        
+        VolumeValue = 0.5f;
+        QualityValue = 1;
+
+        MuteToggleEnabled = false;
+        VibrateToggleEnabled = true;
+        Debug.Log("플레이어 데이터 불러오기");
     }
+
+    private void UpdateUI()
+    {
+        QualitySlider.value = QualityValue;
+        VolumeSlider.value = VolumeValue;
+        MuteToggle.isOn = MuteToggleEnabled;
+        VibrateToggle.isOn = VibrateToggleEnabled;
+    }
+
+    private void SetData()
+    {
+        Debug.Log("설정된 데이터 저장");
+        //혹은 즉시 값 변경
+    }
+
+    public void CloseButtonPressed()
+    {
+        //설정 저장
+        //gameObject.SetActive(false);
+        BG.SetActive(false);
+    }
+
+    public void ResetDataButtonPressed()
+    {
+        Debug.Log("데이터 초기화");
+    }
+
+    public void MuteTogglePressed()
+    {
+        MuteToggleEnabled = MuteToggle.isOn;
+        if (MuteToggleEnabled)
+        {
+            VolumeSlider.enabled = false;
+        }
+        else
+        {
+            VolumeSlider.enabled = true;
+        }
+    }
+
+    public void VolumValueChanged()
+    {
+        VolumeValue = VolumeSlider.value;
+    }
+
+    public void VibrateTogglePressed()
+    {
+        VibrateToggleEnabled = VibrateToggle.isOn;
+    }
+
+    public void QualityValueChanged()
+    {
+        QualityValue = (int)QualitySlider.value;
+        //QualitySettings.SetQualityLevel(QualityValue, true);  실제 품질 조정
+    }
+
+ 
 }
